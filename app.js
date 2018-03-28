5e714926615cf57eea0e50cb08fdd75695d15a26
Anand <karanianandkumar@yahoo.com>
2018-03-28 08:19:56 +0530

    Reading commandline args

--- app.js
+++ app.js
@@ -2,3 +2,17 @@ const fs=require('fs');
 const _=require('lodash');
 
 
+var command=process.argv[2];
+console.log(process.argv);
+
+if(command=="add"){
+    console.log("Adding new Note")
+}else if(command=="list"){
+    console.log("Print List of Notes");
+}else if(command=="read"){
+    console.log("Read the Notes");
+}else if(command=="remove"){
+    console.log("Delete Notes");
+}else{
+    console.log("Command not recognised");   
+}
